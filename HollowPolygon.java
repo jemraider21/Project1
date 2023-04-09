53f6fe52279ce718a766a48d11da0c84341468fa
Jared <jemraider21@gmail.com>
2023-04-09 09:55:16 -0400

    Uploading Skeleton Code

---
+++ HollowPolygon.java
@@ -0,0 +1,17 @@
+// CMSC 330 Advanced Programming Languages
+// Project 1 Skeleton
+// UMGC CITE
+// Spring 2023
+
+import java.awt.*;
+
+// Abstract class that defines all hollow polygons
+
+abstract class HollowPolygon extends Polygon_ {
+
+    // Constructor that calls super constructor
+
+    public HollowPolygon(Color color, int vertexCount) {
+        super(color, vertexCount);
+    }
+}
